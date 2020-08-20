3c06ea711bf71c572ac17b93cebd25a50237e36e
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2020-08-20 14:16:06 +0200

    refactor(hierholzer): variable names

--- snowymontreal/hierholzer.py
+++ snowymontreal/hierholzer.py
@@ -1,7 +1,7 @@
-def hierholzer(M2, path, circuit, cur, n):
+def hierholzer(deg_matrix, path, circuit, cur, n):
     path.append(cur)
     for j in range(n):
-        if M2[cur][j] != 0:
-            M2[cur][j] -= 1
-            hierholzer(M2, path, circuit, j, n)
+        if deg_matrix[cur][j] != 0:
+            deg_matrix[cur][j] -= 1
+            hierholzer(deg_matrix, path, circuit, j, n)
     circuit.insert(0, path.pop())
