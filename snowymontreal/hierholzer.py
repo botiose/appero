140033a02e3b37b8fd5f47cfb9cbeb80a39b4bbc
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2020-08-21 15:47:12 +0200

    doc(hierholzer): docstring

--- snowymontreal/hierholzer.py
+++ snowymontreal/hierholzer.py
@@ -1,9 +1,23 @@
-def hierholzer(deg_matrix, is_oriented, path, circuit, cur, n):
+def hierholzer(deg_matrix, is_oriented, path, circuit, cur, num_vertices):
+    """
+    Recursive implementation of hierholzer's algorithm.
+
+    Given an adjancency matrix corresponding to a eulerian graph this algorithm
+    finds the eulerian cycle of this one.
+
+    Parameters
+    deg_matrix (matrix): Ddjacency matrix
+    is_oriented (bool): Whether the graph is oriented
+    path (vector): Path to current vertex
+    circuit (vector): Currently built eulerian path
+    cur (int): Current vertex
+    num_vertices (int): Number of vertices in the graph
+    """
     path.append(cur)
-    for j in range(n):
+    for j in range(num_vertices):
         if deg_matrix[cur][j] != 0:
             deg_matrix[cur][j] -= 1
             if not is_oriented:
                 deg_matrix[j][cur] -= 1
-            hierholzer(deg_matrix, is_oriented, path, circuit, j, n)
+            hierholzer(deg_matrix, is_oriented, path, circuit, j, num_vertices)
     circuit.insert(0, path.pop())
