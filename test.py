e40351798bbe12fcb3098cf9062d74fab245072d
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2020-08-21 14:58:13 +0200

    test(solve): undirected graphs

--- test.py
+++ test.py
@@ -6,10 +6,14 @@ from snowymontreal import solve
 import sys
 
 num_vertices = 4
+is_oriented = True
 
 while True:
     H = nx.gnp_random_graph(num_vertices, 0.7, directed=True)
-    G = nx.MultiDiGraph()
+    if is_oriented:
+        G = nx.MultiDiGraph()
+    else:
+        G = nx.MultiGraph()
     i = 0
     for edge in H.edges(data=True):
         G.add_edge(edge[0], edge[1], distance=random.randint(0,100), id=i)
@@ -27,18 +31,25 @@ while True:
     for edge in cpp_circuit:
         tot2 += edge[3]['distance']
 
-    my_circuit = solve(True, num_vertices, edge_list)
+    my_circuit = solve(is_oriented, num_vertices, edge_list)
     tot1 = 0
     prev_vert = my_circuit[0]
     for cur_vert in my_circuit[1:]:
         tot1 += G[prev_vert][cur_vert][0]['distance']
         prev_vert = cur_vert
 
-    if tot1 != tot2:
-        print("test.py: Mismatch. given: " + str(tot1) + ", expected: "
-              + str(tot2))
-        for edge in G.edges():
-            print(edge)
-        quit()
+    if is_oriented:
+        if tot1 != tot2:
+            print("test.py: Mismatch. given: " + str(tot1) + ", expected: "
+                  + str(tot2))
+            for edge in G.edges(data=True):
+                print(edge)
+                quit()
     else:
-        print("test.py: Results match")
+        if len(my_circuit) != len(cpp_circuit) + 1:
+            print("test.py: Mismatch. given: " + str(len(my_circuit)) +
+                  ", expected: " + str(len(cpp_circuit) + 1));
+            print(my_circuit)
+            print(cpp_circuit)
+            quit()
+    print("test.py: Results match")
