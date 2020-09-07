95926172d31b7b5e4d3360b6baba3b8409119f35
MLTDK <maxime.aupetitalot@epita.fr>
2020-09-07 18:02:54 +0200

    snowymontreal(floyd-warshall): corrections to the parameters

--- snowymontreal/floyd_warshall_alt.py
+++ snowymontreal/floyd_warshall_alt.py
@@ -21,7 +21,7 @@ def adjacency_matrix(verts, edges):
                 
     return Adjs
 
-def floyd_warshall(verts, edges):
+def floyd_warshall(Adjs, verts):
     """
     Alternative version of the Floyd-Warshall algorithm
 
@@ -29,43 +29,6 @@ def floyd_warshall(verts, edges):
 
     Minimum paths are calculated in-place
     """
-    Fwmat = adjacency_matrix(verts, edges)
-    
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                Fwmat[i][j] = minimum(Fwmat[i][j], Fwmat[i][k] + Fwmat[k][j])
-                    
-    return Fwmat
-
-n = 5
-edges = [(0,1,1), (1,0,3), (3,2,1), (1,4,4), (4,3,-1), (3,4,2)]
-print(floyd_warshall(n, edges))
-
-def minimum(a, b):
-    if a < b:
-        return a
-    else:
-        return b
-
-def adjacency_matrix(verts, edges):
-    Adjs = []
-    
-    for i in range(verts):
-        Adjs.append([])
-        
-        for j in range(verts):
-            weight = float('inf')
-            
-            for edge in edges:
-                if i == edge[0] and j == edge[1]:
-                    weight = edge[2]
-                    
-            Adjs[i].append(weight)
-                
-    return Adjs
-
-def floyd_warshall(Adjs, verts):
     Fwmat = Adjs
     
     for k in range(verts):
